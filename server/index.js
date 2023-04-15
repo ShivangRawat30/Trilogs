8e203c21a351bc78f59683721a424f5a0be0436c
Shivang Rawat <rawatshivang30@.com>
2023-04-15 18:22:06 +0530

    commit2

--- server/index.js
+++ server/index.js
@@ -10,7 +10,13 @@ import path from "path";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/authRoute.js";
 import userRoutes from "./routes/userRoute.js";
+import postRoutes from "./routes/postRoute.js";
 import { register } from "./controllers/authController.js";
+import { createPost } from "./controllers/postController.js";
+import { verifyToken } from "./middleware/authMiddleware.js";
+import User from "./models/User.js";
+import Post from "./models/Post.js";
+import { users, posts } from "./data/index.js";
 
 // CONFIGURATIONS
 const __filename = fileURLToPath(import.meta.url);
@@ -39,10 +45,12 @@ const upload = multer({ storage });
 
 // ROUTES WITh FILES
 app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
 // ROUTES
 app.use("/auth", authRoutes);
 app.use("/user", userRoutes);
+app.use("/posts", postRoutes);
 
 // MONGOOSE SETUP
 const PORT = process.env.PORT || 6001;
@@ -56,5 +64,9 @@ mongoose
   )
   .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+    // ADD DATA ONE TIME
+    // User.insertMany(users);
+    // Post.insertMany(posts);
   })
   .catch((error) => console.log(`${error} did not connect`));
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,7 +1,7 @@
 import express from "express";
 import {
   getUser,
-  getUserFrinds,
+  getUserFriends,
   addRemoveFriend,
 } from "../controllers/userController.js";
 import { verifyToken } from "../middleware/authMiddleware.js";
@@ -10,3 +10,9 @@ const router = express.Router();
 
 // READ
 router.get("/:id", verifyToken, getUser);
+router.get("/:id/friends", verifyToken, getUserFriends);
+
+// UPDATE
+router.patch("/:id/:friendId", verifyToken, addRemoveFriend);
+
+export default router;
