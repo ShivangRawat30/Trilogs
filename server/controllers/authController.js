fab29883bb640112b9c9944260b6b01b6c004578
Shivang Rawat <rawatshivang30@gmail.com>
2023-04-18 23:37:20 +0530

    commit

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -2,7 +2,7 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import User from "../models/User.js";
 
-// REGISTER USER
+/* REGISTER USER */
 export const register = async (req, res) => {
   try {
     const {
@@ -38,15 +38,15 @@ export const register = async (req, res) => {
   }
 };
 
-// LOGGING IN
+/* LOGGING IN */
 export const login = async (req, res) => {
   try {
     const { email, password } = req.body;
-    const user = await User.findOne({ email });
-    if (!user) return res.status(400).json({ msg: "User does not exist..." });
+    const user = await User.findOne({ email: email });
+    if (!user) return res.status(400).json({ msg: "User does not exist. " });
 
     const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) return res.status(400).json({ msg: "Invalid credentials.." });
+    if (!isMatch) return res.status(400).json({ msg: "Invalid credentials. " });
 
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
     delete user.password;
