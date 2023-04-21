5123ddccacbdc8849abb48fbc83d49862473aeb8
Shivang Rawat <rawatshivang30@gmail.com>
2023-04-22 03:41:31 +0530

    f

--- client/src/scenes/navbar/index.js
+++ client/src/scenes/navbar/index.js
@@ -57,7 +57,7 @@ const Navbar = () => {
             },
           }}
         >
-          Sociopedia
+          Trilogs
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween
